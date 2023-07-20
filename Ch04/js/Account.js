6025ce73ed1c19db19b1e51cd92b938d457e0681
more5265 <more5265@gmail.com>
2023-07-20 17:22:53 +0900

    7/20 예제실습

---
+++ Ch04/js/Account.js
@@ -0,0 +1,30 @@
+class Account {
+
+    constructor(bank, id, name, balance){ //  생성자에서 속성 생성
+        this.bank = bank;
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    deposit(money) {
+        this.balance += money;
+
+    }
+
+    withdraw(money){
+        this.balance -= money;
+    }
+
+    show() {
+        document.write('<p>');
+        document.write('은행명 : ' + this.bank + '<br>');
+        document.write('계좌번호 : ' + this.id + '<br>');
+        document.write('입금주 : ' + this.name + '<br>');
+        document.write('현재잔액 : ' + this.balance + '<br>');
+        document.write('<p>');
+    }
+
+
+
+}
